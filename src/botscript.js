b7a7f10986763c8b682c1f014229221b2173f3ce
Rahul-4480 <rahul.int24@infraspec.dev>
2024-02-28 18:34:36 +0530

    Add simple working code

--- src/botscript.js
+++ src/botscript.js
@@ -1,1 +1,20 @@
-console.log("hello");
+require('dotenv').config();
+
+const axios = require('axios');
+const { Client, GatewayIntentBits } = require('discord.js'); // Fix here
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }); // Fix here
+
+client.on('ready', () => {
+    console.log('bot is ready');
+});
+
+client.on('messageCreate', async (message) => { // Fix here
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong',
+        });
+    } 
+});
+
+client.login(process.env.DISCORD_TOKEN);
