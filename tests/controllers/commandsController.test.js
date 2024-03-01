dee4a1067e92f562d71753630100033d5cf65561
Rahul-4480 <rahul.int24@infraspec.dev>
2024-03-01 10:36:11 +0530

    Implement test case for get available books

--- tests/controllers/commandsController.test.js
+++ tests/controllers/commandsController.test.js
@@ -1,5 +1,6 @@
-const {start} = require('../../src/controllers/commandsController')
-const constants=require('../../src/constants/constant')
+const { start } = require('../../src/controllers/commandsController')
+const { getAvailableBooks } = require('../../src/controllers/commandsController')
+const constants = require('../../src/constants/constant')
 describe('/start command', () => {
     afterEach(() => {
         jest.clearAllMocks();
@@ -15,5 +16,69 @@ describe('/start command', () => {
 
         expect(mockMessage.reply).toHaveBeenCalledWith(`${constants.WELCOME_MESSAGE}, TestUser!`);
         expect(mockMessage.reply).toHaveBeenCalledWith(constants.MENU_OPTIONS);
+    });
+});
+
+describe('getAvailableBooks', () => {
+    let mockMessage;
+    let mockConnection;
+
+    beforeEach(() => {
+        mockMessage = {
+            reply: jest.fn(),
+        };
+
+        mockConnection = {
+            query: jest.fn(),
+        };
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('should reply with available books when there are books', () => {
+        const mockResults = [
+            { id: 1, title: 'Book 1', quantity_available: 3 },
+            { id: 2, title: 'Book 2', quantity_available: 5 },
+        ];
+
+        mockConnection.query.mockImplementationOnce((query, callback) => {
+            callback(null, mockResults);
+        });
+
+        getAvailableBooks(mockMessage, mockConnection);
+
+        expect(mockMessage.reply).toHaveBeenCalledWith(
+            expect.stringContaining("Available Books:\n- Book 1\n- Book 2")
+        );
+    });
+
+    test('should reply with "No available books found" when there are no books', () => {
+        const mockResults = [];
+
+        mockConnection.query.mockImplementationOnce((query, callback) => {
+            callback(null, mockResults);
+        });
+
+        getAvailableBooks(mockMessage, mockConnection);
+
+        expect(mockMessage.reply).toHaveBeenCalledWith(
+            'No available books found.'
+        );
+    });
+
+    test('should reply with "Error fetching available books" when there is an error', () => {
+        const mockError = new Error('Test error');
+
+        mockConnection.query.mockImplementationOnce((query, callback) => {
+            callback(mockError, null);
+        });
+
+        getAvailableBooks(mockMessage, mockConnection);
+
+        expect(mockMessage.reply).toHaveBeenCalledWith(
+            'Error fetching available books. Please try again later.'
+        );
     });
 });
