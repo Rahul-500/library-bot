2869947b1af5821645b32a8f3802de5c76fddc42
Rahul-4480 <rahul.int24@infraspec.dev>
2024-03-13 11:53:39 +0530

    Add updateBookDetails method

--- src/service/databaseService.js
+++ src/service/databaseService.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const transactions = require('../service/transactions')
 const { DB_NAME, TABLE_NAME_BOOKS } = process.env;
+const constants = require('../constants/constant')
 
 exports.addBookToDatabase = async (message, connection, bookDetails) => {
 
@@ -26,7 +27,6 @@ exports.addBookToDatabase = async (message, connection, bookDetails) => {
         await transactions.commitTransaction(connection);
 
     } catch (error) {
-
         await transactions.rollbackTransaction(connection);
         message.reply("An unexpected error occurred while processing the command.");
     }
@@ -57,5 +57,37 @@ exports.deleteBookWithQuantity = async (message, connection, book, quantity) => 
     } catch (error) {
         await transactions.rollbackTransaction(connection);
         message.reply("An unexpected error occurred while processing the command.");
+    }
+}
+
+exports.updateBookDetails = async (message, connection, book, title, author, publishedYear, quantity) => {
+    try {
+        await transactions.beginTransaction(connection)
+        const QUERY = `
+                UPDATE ${DB_NAME}.${TABLE_NAME_BOOKS}
+                SET title = '${title}', 
+                    author = '${author}', 
+                    published_year = ${publishedYear}, 
+                    quantity_available = ${quantity}
+                WHERE id = ${book.id};
+            `;
+
+        const queryPromise = new Promise((resolve, reject) => {
+            connection.query(QUERY, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+        const updatedResult = await queryPromise
+
+        await transactions.commitTransaction(connection);
+        
+        message.reply(constants.BOOK_UPDATED_MESSAGE);
+    } catch (error) {
+        await transactions.rollbackTransaction(connection);
+        message.reply(constants.ERROR_UPDATE_BOOK_MESSAGE);
     }
 }
--- tests/service/databaseService.test.js
+++ tests/service/databaseService.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const sinon = require('sinon');
 const bookService = require('../../src/service/databaseService');
 const transactions = require('../../src/service/transactions');
+const constants = require('../../src/constants/constant')
 
 describe('addBookToDatabase', () => {
     let mockMessage;
@@ -110,4 +111,60 @@ describe('deleteBookWithQuantity', () => {
     });
 });
 
+describe('updateBookDetails', () => {
+    let mockMessage;
+    let mockConnection;
+    let mockBook;
+    let mockTitle;
+    let mockAuthor;
+    let mockPublishedYear;
+    let mockQuantity;
 
+    beforeEach(() => {
+        mockMessage = { reply: sinon.spy() };
+        mockConnection = {
+            query: sinon.stub(),
+        };
+        mockBook = { id: 1, title: 'Test Book', author: 'Test Author', published_year: 2022, quantity_available: 5 };
+        mockTitle = 'Updated Test Book';
+        mockAuthor = 'Updated Test Author';
+        mockPublishedYear = 2023;
+        mockQuantity = 10;
+    });
+
+    it('should update book details successfully', async () => {
+        const beginTransactionStub = sinon.stub(transactions, 'beginTransaction').resolves();
+        const commitTransactionStub = sinon.stub(transactions, 'commitTransaction').resolves();
+        const rollbackTransactionStub = sinon.stub(transactions, 'rollbackTransaction').resolves();
+        mockConnection.query.callsArgWith(1, null, {}); // simulate successful query
+
+        await bookService.updateBookDetails(mockMessage, mockConnection, mockBook, mockTitle, mockAuthor, mockPublishedYear, mockQuantity);
+
+        assert.ok(beginTransactionStub.calledOnce);
+        assert.ok(commitTransactionStub.calledOnce);
+        assert.ok(!rollbackTransactionStub.called);
+        assert.ok(mockMessage.reply.calledWith(constants.BOOK_UPDATED_MESSAGE));
+
+        beginTransactionStub.restore();
+        commitTransactionStub.restore();
+        rollbackTransactionStub.restore();
+    });
+
+    it('should handle errors and rollback transaction', async () => {
+        const beginTransactionStub = sinon.stub(transactions, 'beginTransaction').resolves();
+        const commitTransactionStub = sinon.stub(transactions, 'commitTransaction').resolves();
+        const rollbackTransactionStub = sinon.stub(transactions, 'rollbackTransaction').resolves();
+        mockConnection.query.callsArgWith(1, new Error('Fake database error'));
+
+        await bookService.updateBookDetails(mockMessage, mockConnection, mockBook, mockTitle, mockAuthor, mockPublishedYear, mockQuantity);
+
+        assert.ok(beginTransactionStub.calledOnce);
+        assert.ok(!commitTransactionStub.called);
+        assert.ok(rollbackTransactionStub.calledOnce);
+        assert.ok(mockMessage.reply.calledWith(constants.ERROR_UPDATE_BOOK_MESSAGE));
+
+        beginTransactionStub.restore();
+        commitTransactionStub.restore();
+        rollbackTransactionStub.restore();
+    });
+});
