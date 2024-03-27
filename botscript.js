df52522374157a79e746fea312bde3fa65136d02
rohit-ng <rohit.int24@infraspec.dev>
2024-03-27 15:58:02 +0530

    Refactor botscript file

--- botscript.js
+++ botscript.js
@@ -21,48 +21,53 @@ const client = new Client({
 });
 const userEventsMap = new Map();
 
-connect()
-  .then((connection) => {
-    client.on("ready", () => {
-      console.log(`Logged in as ${client.user.tag}!`);
-      checkOverdueBooks({ connection, client });
-    });
+const startBot = async () => {
+  let connection;
 
-    const messageCreateHandler = (message) => {
-      if (message.author.bot || !message.channel.type) return;
-      if (message.guild) {
-        const isGeneralChannelOrThread =
-          message.channel.isThread() || message.channel.name === "general";
-        if (isGeneralChannelOrThread) return;
-      }
-
-      const authorId = message.author.id;
-      if (!userEventsMap.has(authorId)) {
-        userEventsMap.set(authorId, { messageCreate: true });
-      }
-      if (!userEventsMap.get(authorId).messageCreate) {
-        return;
-      }
-
-      const dependencies = {
-        message,
-        commandsController,
-        connection,
-        validateUser,
-        bookMap,
-        checkedOutBooks,
-        display,
-        userEventsMap,
-        client,
-      };
-      menu(dependencies);
-    };
-
-    client.on("messageCreate", messageCreateHandler);
-
-    client.login(process.env.DISCORD_TOKEN);
-  })
-  .catch((error) => {
-    console.error("Error connecting to MySQL database:", error);
+  try {
+    connection = await connect();
+    console.log("Connected to MySQL database successfully!");
+  } catch (error) {
+    console.error("Error: Failed to connect to MySQL database");
     process.exit(-1);
+  }
+
+  client.once("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    checkOverdueBooks({ connection, client });
   });
+
+  const messageCreateHandler = (message) => {
+    if (message.author.bot || !message.guild) return;
+    const isGeneralChannelOrThread =
+      message.channel.isThread() || message.channel.name === "general";
+    if (isGeneralChannelOrThread) return;
+
+    const authorId = message.author.id;
+    if (!userEventsMap.has(authorId)) {
+      userEventsMap.set(authorId, { messageCreate: true });
+    }
+    if (!userEventsMap.get(authorId).messageCreate) {
+      return;
+    }
+
+    const dependencies = {
+      message,
+      commandsController,
+      connection,
+      validateUser,
+      bookMap,
+      checkedOutBooks,
+      display,
+      userEventsMap,
+      client,
+    };
+    menu(dependencies);
+  };
+
+  client.on("messageCreate", messageCreateHandler);
+
+  await client.login(process.env.DISCORD_TOKEN);
+};
+
+startBot();
