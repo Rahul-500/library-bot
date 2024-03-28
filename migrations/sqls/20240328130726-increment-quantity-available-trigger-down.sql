68f6e5b1fe63d78695e352e0d7870eddf6e6304d
rohit-ng <rohit.int24@infraspec.dev>
2024-03-28 18:38:48 +0530

    Add increment quantity available trigger

---
+++ migrations/sqls/20240328130726-increment-quantity-available-trigger-down.sql
@@ -0,0 +1,1 @@
+DROP TRIGGER IF EXISTS `increment_quantity_available`;
---
+++ migrations/sqls/20240328130726-increment-quantity-available-trigger-up.sql
@@ -0,0 +1,10 @@
+
+CREATE TRIGGER `increment_quantity_available` BEFORE DELETE ON `issued_books`
+FOR EACH ROW
+BEGIN
+    DECLARE quantity_to_increment INT;
+
+    SET quantity_to_increment = 1;
+
+    UPDATE books SET quantity_available = quantity_available + quantity_to_increment WHERE id = OLD.book_id;
+END
